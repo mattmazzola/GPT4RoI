6106e4b3b5ac8fd4094ccc09a10f40b4a88b535e
Matthew Mazzola <mattm@microsoft.com>
2024-01-05 18:34:19 +0000

    feat: automate nvm, node, pnpm, gradio-box install

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -43,10 +43,47 @@ conda install -y -c nvidia -c pytorch pytorch-cuda=11.7 pytorch=1.10.0 torchvisi
 pip install ninja
 pip install flash-attn --no-build-isolation
 
+MMCV_WITH_OPS=1 pip install -e mmcv-1.4.7
+
+git clone https://huggingface.co/jeffwan/llama-7b-hf ./llama-7b
+git clone https://huggingface.co/shilongz/GPT4RoI-7B-delta-V0 ./GPT4RoI-7B-delta
+
+export PYTHONPATH=`pwd`:$PYTHONPATH
+python3 -m scripts.apply_delta \
+    --base ./llama-7b \
+    --target ./GPT4RoI-7B \
+    --delta ./GPT4RoI-7B-delta
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm install 18.16.0
+
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+
+export PNPM_HOME="/home/vscode/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
+source ~/.zshrc
+
+pnpm --version
+
+git clone https://github.com/ShoufaChen/gradio-dev.git
+cd gradio-dev
+bash scripts/build_frontend.sh
+pip install -e .
+cd ..
+
+node -v
+npm -v
 nvcc -V
 TORCH_VERSION=$(python -c "import torch;print(torch.__version__)")
 echo "Torch version: $TORCH_VERSION"
-
-MMCV_WITH_OPS=1 pip install -e mmcv-1.4.7
 
 echo "postCreateCommand.sh completed!"
